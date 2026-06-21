57b0fd571bfcbd9771309bbd66f207f7e8e96007
sachintha2882 <sachinthadhitinayaka@gmail.com>
2026-06-21 09:44:23 +0530

    initialized the HMS for UOR

---
+++ src/test/java/com/example/HostalManagementSystem_UOR/HostalManagementSystemUorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HostalManagementSystem_UOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalManagementSystemUorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
