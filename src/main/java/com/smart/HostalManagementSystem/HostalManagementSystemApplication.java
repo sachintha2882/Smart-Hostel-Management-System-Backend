4b9ffb8cf9b0f47bb7280f3ecdf50ff00d673ee8
sachintha2882 <sachinthadhitinayaka@gmail.com>
2026-07-14 22:30:01 +0530

    intialized the back-end

---
+++ src/main/java/com/smart/HostalManagementSystem/HostalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smart.HostalManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/HostalManagementSystem/HostalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.HostalManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
